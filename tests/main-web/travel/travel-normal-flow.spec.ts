91bf0c207dbbfa427627f1d47ce88b7e188032e2
DAR0306003NB\sittikorn <mu.sittikorn@gmail.com>
2025-05-26 21:59:01 +0700

    test

--- tests/main-web/travel/travel-normal-flow.spec.ts
+++ tests/main-web/travel/travel-normal-flow.spec.ts
@@ -19,7 +19,7 @@ test(
     commonTQM,
   }) => {
     await travelIndex.goto(data.travelSearchWizard);
-    //123
+    //456
     // await travelIndex.searchTravel(data.travelSearchWizard);
     // await expect(page).toHaveURL(
     //   process.env.MAIN_WEB_URL + "/travel-insurance/filter"
